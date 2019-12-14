2668194aa67f6288acbda149e2193c6ba7f36530
Pramods <Pramods@Pramod>
2019-12-15 02:27:30 +0530

    MultiClass

---
+++ JDC003/src/pkg02/MCA001.java
@@ -0,0 +1,9 @@
+package pkg02;
+
+public class MCA001 {
+	public static void main(String args[]){
+		MCA002 tstOb = new MCA002();
+		tstOb.tMsg();
+	}
+
+}
---
+++ JDC003/src/pkg02/MCA002.java
@@ -0,0 +1,7 @@
+package pkg02;
+
+public class MCA002 {
+	public void tMsg(){
+		System.out.println("This msg is from another class");
+	}
+}
