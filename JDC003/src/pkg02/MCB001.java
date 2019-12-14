15e13228de68742fe3760fc3c3c7b2c70b1130f2
Pramods <Pramods@Pramod>
2019-12-15 02:33:41 +0530

    MultiClass

---
+++ JDC003/src/pkg02/MCB001.java
@@ -0,0 +1,8 @@
+package pkg02;
+
+public class MCB001 {
+	public static void main(String args[]){
+		MCB002 tob=new MCB002();
+		tob.inMsg();
+	}
+}
---
+++ JDC003/src/pkg02/MCB002.java
@@ -0,0 +1,7 @@
+package pkg02;
+
+public class MCB002 {
+	public void inMsg(){
+		System.out.println("This is another test message");
+	}
+}
