36ab08be096bf31f823ce187a4d0f25f0ca37d83
Prad <prad.j.life@gmail.com>
2019-12-15 01:54:55 +0530

    multiclasses

---
+++ JDC003/src/pkg02/MCCode01.java
@@ -0,0 +1,8 @@
+package pkg02;
+
+public class MCCode01 {
+	public static void main(String args[]){
+		MCCode02 OneObj = new MCCode02();
+		OneObj.msg();
+	}
+}
---
+++ JDC003/src/pkg02/MCCode02.java
@@ -0,0 +1,7 @@
+package pkg02;
+
+public class MCCode02 {
+	public void msg(){
+		System.out.println("This is test message for multiclass");
+	}
+}
