f7bf12998b66c8dcce40b3fe5c7291b971f77c3f
Prad <prad.j.life@gmail.com>
2019-12-19 02:25:10 +0530

    Sum

---
+++ JDC003/src/pkg02/SumTest01.java
@@ -0,0 +1,16 @@
+package pkg02;
+import java.util.Scanner;
+public class SumTest01 {
+	public static void main(String args[]){
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter a");
+	int  a = s.nextInt();
+	System.out.println("Enter b");
+	int  b = s.nextInt();
+	
+	int c=a+b;
+	
+	System.out.println(c);
+	}
+
+}
