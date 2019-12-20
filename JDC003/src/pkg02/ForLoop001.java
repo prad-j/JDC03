4cc6f96216d90940a1f43341073fa30efcf93d58
Prad <prad.j.life@gmail.com>
2019-12-21 01:22:57 +0530

    For

---
+++ JDC003/src/pkg02/ForLoop001.java
@@ -0,0 +1,10 @@
+package pkg02;
+
+public class ForLoop001 {
+	public static void main(String args[]){
+		for (int cnt=0;cnt<=10;cnt++){
+			System.out.println(cnt);
+		}
+	}
+
+}
---
+++ JDC003/src/pkg02/ForLoop002.java
@@ -0,0 +1,10 @@
+package pkg02;
+
+public class ForLoop002 {
+	public static void main(String args[]){
+		for(int c=0;c<50;c+=10){
+			System.out.println(c);
+		}
+	}
+
+}
