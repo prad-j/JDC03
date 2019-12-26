7d8f41b6557a05d28650fb73264e75dcf65d9da3
Prad <prad.j.life@gmail.com>
2019-12-27 00:13:05 +0530

    Array 3 musketeers

---
+++ JDC003/src/pkg02/Array002.java
@@ -0,0 +1,12 @@
+package pkg02;
+
+public class Array002 {
+	public static void main(String args[]){
+		
+		String name[]={"Athos","Porthos","Aramis"};
+		
+		System.out.println(name[0]+", "+name[1]+" & "+name[2]+" are three Musketeers.");
+		
+	}
+
+}
