2cad9ec114e7000a0e98295d533a08548727b20b
Prad <prad.j.life@gmail.com>
2019-12-27 01:01:27 +0530

    ArrayTble001

---
+++ JDC003/src/pkg02/ArrayTable001.java
@@ -0,0 +1,14 @@
+package pkg02;
+
+public class ArrayTable001 {
+	public static void main(String args[]){
+		System.out.println("Index\tvalue");
+		
+		int ac[]={25, 10, 39, 88, 78, 98};
+		
+		for(int counter=0;counter<ac.length;counter++){
+			System.out.println(1+counter+"\t"+ac[counter]);
+		}
+	}
+
+}
