ed93f8ca38edcc87b264f064c564b13398c71dd7
Prad <prad.j.life@gmail.com>
2019-12-26 16:39:02 +0530

    do while

---
+++ JDC003/src/pkg02/DoWhile002.java
@@ -0,0 +1,11 @@
+package pkg02;
+
+public class DoWhile002 {
+	public static void main(String args[]){
+		int counter=0;
+		do{
+			System.out.println(counter);
+			counter++;
+		}while(counter<10);
+	}
+}
