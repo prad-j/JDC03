c61eb44a1fa0edda491dd5e3c4e2955afb669959
Prad <prad.j.life@gmail.com>
2019-12-26 23:57:29 +0530

    Array001

---
+++ JDC003/src/pkg02/Array001.java
@@ -0,0 +1,17 @@
+package pkg02;
+
+public class Array001 {
+	public static void main(String args[]){
+		
+		int numa[]=new int[10];
+		
+		numa[0]=25;
+		numa[1]=-60;
+		numa[5]=40;
+		numa[9]=28;
+		
+		System.out.println(numa[1]);
+		System.out.println(numa[5]+","+numa[9]);
+		
+	}
+}
