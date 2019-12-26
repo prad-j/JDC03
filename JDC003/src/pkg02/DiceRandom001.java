59e61da45df6f699ae5d4f4d0f3183bba5e92adf
Prad <prad.j.life@gmail.com>
2019-12-26 23:39:58 +0530

    Modified

--- JDC003/src/pkg02/DiceRandom001.java
+++ JDC003/src/pkg02/DiceRandom001.java
@@ -9,6 +9,8 @@ public class DiceRandom001 {
 		
 		for (int c=0;c<=10;c++){
 			a=1+dice.nextInt(6);
+			/*if a=dice.nextLine(6) is used by default it
+			 starts taking values from 0 to 5*/
 			System.out.println(a);
 		}
 		
