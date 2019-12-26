d6fe47f024157ba00ce4f602f9621e3d3c86f3bd
Prad <prad.j.life@gmail.com>
2019-12-27 01:22:58 +0530

    ArrayTble2Array

--- JDC003/src/pkg02/ArrayStrTble001.java
+++ JDC003/src/pkg02/ArrayStrTble001.java
@@ -5,9 +5,10 @@ public class ArrayStrTble001 {
 		System.out.println("RollNo\tName");
 		
 		String name[]={"Sam","Peter","Maggi","Lucy","Stella"};
+		int num[]={1, 2, 3, 4, 5};
 		
 		for(int counter=0;counter<name.length;counter++){
-			System.out.println(""+counter+"\t"+name[counter]);
+			System.out.println(""+num[counter]+"\t"+name[counter]);
 		}
 		
 	}
