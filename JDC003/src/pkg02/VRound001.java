2f2d5bc57a7975ba8dbbb666b5bc05f78ab2e5c6
Prad <prad.j.life@gmail.com>
2019-12-26 17:30:39 +0530

    ValueRound01

---
+++ JDC003/src/pkg02/VRound001.java
@@ -0,0 +1,12 @@
+package pkg02;
+
+public class VRound001 {
+	public static void main(String args[]){
+		System.out.println(Math.round(24.49));
+		System.out.println(Math.round(24.50));
+		System.out.println(Math.round(24.51));
+		System.out.println(Math.round(24.99));
+		System.out.println(Math.round(24.01));
+	}
+
+}
