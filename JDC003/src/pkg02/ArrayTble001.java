3c9913e33ebe7afe218e85ed0d2a744e9a49b26b
Prad <prad.j.life@gmail.com>
2019-12-29 23:46:53 +0530

    TbleArray

---
+++ JDC003/src/pkg02/ArrayTble001.java
@@ -0,0 +1,13 @@
+package pkg02;
+
+public class ArrayTble001 {
+	public static void main(String args[]){
+		System.out.println("Index\tValue");
+		int in[]={150, 150, 150, 96, 94, 93};
+		
+		for (int counter=0;counter<in.length;counter++){
+			System.out.println(" "+counter+"\t "+in[counter]);
+		}
+	}
+	
+}
