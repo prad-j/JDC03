8df824c0a9367d9752a3067b2b4f385944d74871
Prad <prad.j.life@gmail.com>
2019-12-30 00:12:57 +0530

    AvgInArray

--- JDC003/src/pkg02/ArrayTble002.java
+++ JDC003/src/pkg02/ArrayTble002.java
@@ -2,7 +2,11 @@ package pkg02;
 
 public class ArrayTble002 {
 	public static void main(String args[]){
-		System.out.println("Subject name \t Marks");
+		double total =0;
+		double percent=0;
+		
+		System.out.println("Subject name\tMarks");
+		System.out.println("-----------------------");
 		
 		String snm[]={"Physics","Chemistry","Mathematics","Electronics","English"};
 		int grd[]={100, 100, 100, 200, 96};
@@ -10,5 +14,15 @@ public class ArrayTble002 {
 		for(int c=0;c<snm.length;c++){
 			System.out.println(snm[c]+" \t"+grd[c]);
 		}
+		for (int c=0;c<grd.length;c++){
+			total=total+grd[c];
+		}
+		
+		percent=(100*total)/600;
+		
+		System.out.println();
+		System.out.println("Total number of marks obtained are "+total+".");
+		System.out.println();
+		System.out.println("The percentage obtained is "+percent+".");
 	}
 }
