fe346a7a7ad2cb73d35c9c8b43e0082c0bce563c
Prad <prad.j.life@gmail.com>
2019-12-29 23:40:01 +0530

    Array Simple

--- JDC003/src/pkg02/Array03.java
+++ JDC003/src/pkg02/Array03.java
@@ -2,10 +2,10 @@ package pkg02;
 
 public class Array03 {
 	public static void main(String args[]){
-		String name[]={"Sam", "Sandra", "Harry", "John"};
+		String name[]={"Sam", "Sandra", "Harry", "John", "Martha"};
 		
 		System.out.println("Name of the members of his family as per age are "
-		+name[0]+", "+name[1]+", "+name[2]+", "+name[3]+".");
+		+name[0]+", "+name[1]+", "+name[2]+", "+name[3]+", "+name[4]);
 	}
 
 }
