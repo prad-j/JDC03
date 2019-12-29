bbe947e6d33f7d6fc772c1f1c4259124bd9e27b0
Pramods <Pramods@Pramod>
2019-12-29 23:37:52 +0530

    Array

---
+++ JDC003/src/pkg02/Array03.java
@@ -0,0 +1,11 @@
+package pkg02;
+
+public class Array03 {
+	public static void main(String args[]){
+		String name[]={"Sam", "Sandra", "Harry", "John"};
+		
+		System.out.println("Name of the members of his family as per age are "
+		+name[0]+", "+name[1]+", "+name[2]+", "+name[3]+".");
+	}
+
+}
