b9ed3dcf31645d6b5a0d902a7bf5f4bdeef74163
Prad <prad.j.life@gmail.com>
2019-12-30 23:42:43 +0530

    PrimeNum01

---
+++ JDC003/src/pkg02/PrimeNum001.java
@@ -0,0 +1,29 @@
+package pkg02;
+
+public class PrimeNum001 {
+	public static void main(String args[]){
+		int n=39;
+		int m;
+		int f =0;
+		
+		m=n/2;
+		
+		if(n==0||n==1){
+			System.out.println(n+" is a prime number.");
+		}else{
+			for(int i=2;i<=m;i++){
+				if(n%i==0){
+					System.out.println(n+" is not a prime number.");
+					f=1;
+					break;
+				}else{
+					
+				}			
+				
+			}if(f==0){
+				System.out.println(n+" is a prime number.");
+			}
+		}
+	}
+	
+}
