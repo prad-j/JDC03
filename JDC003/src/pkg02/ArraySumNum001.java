cf60943c2f25faa1a8421c538f7431740461ef95
Prad <prad.j.life@gmail.com>
2019-12-31 14:31:02 +0530

    Refactor

--- JDC003/src/pkg02/ArraySumNum001.java
+++ JDC003/src/pkg02/ArraySumNum001.java
@@ -3,20 +3,15 @@ package pkg02;
 public class ArraySumNum001 {
 	public static void main(String args[]){
 		int ar[]=new int[2];
-		
-		for(int i=0;i<10;i++){
+	
 			for(int a=0;a<10;a++){
 				ar[0]=a;
 				for(int b=0;b<10;b++){
 					ar[1]=b;
-					for(int c=0;c<1;c++){
 						System.out.println(ar[0]+"\t"+ar[1]);
-				}
-			
 				}
 			}
 			
-		}
 	}
 	
 }
