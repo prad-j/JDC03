1e9c7de9a1f0716e4a82157ef2baa053b684abfb
Prad <prad.j.life@gmail.com>
2020-01-01 18:39:15 +0530

    LuckyNumFind001

--- JDC003/src/pkg02/ArraySumCond002.java
+++ JDC003/src/pkg02/ArraySumCond002.java
@@ -1,11 +1,11 @@
 package pkg02;
 
-public class ArraySumCond001 {
+public class ArraySumCond002 {
 	public static void main(String args[]){
 		int ar[]=new int[4];
 		int total=0;
-		int sum;
 		int n;
+		int sum;
 		
 		for(int a=0;a<10;a++){
 			ar[0]=a;
@@ -17,17 +17,21 @@ public class ArraySumCond001 {
 						ar[3]=d;
 						
 						total=ar[0]+ar[1]+ar[2]+ar[3];
-						
+						if (total==3){
+							System.out.println(ar[0]+" "+ar[1]+" "+ar[2]+" "+ar[3]);
+							System.out.println(total);
+							
+						}
 						if(total>10){
 							n=total;
-								for(sum=0;n!=0;n/=10){
-									sum+=n%10;
-								}
-								if(sum==3){
-									System.out.println(ar[0]+" "+ar[1]+" "+ar[2]+" "+ar[3]);
-									System.out.println(sum);
-									System.out.println();
-								}
+							for(sum=0;n!=0;n/=10){
+								sum+=n%10;
+							}
+							if(sum==3){
+								System.out.println(ar[0]+" "+ar[1]+" "+ar[2]+" "+ar[3]);
+								System.out.println(sum);
+								System.out.println();
+							}
 							
 						}
 					}
@@ -35,5 +39,4 @@ public class ArraySumCond001 {
 			}
 		}
 	}
-
 }
