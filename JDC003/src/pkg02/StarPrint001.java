87e8b04be61f35a547171fc0e97a0355b477eed7
Prad <prad.j.life@gmail.com>
2020-01-04 00:28:09 +0530

    FunStar001

---
+++ JDC003/src/pkg02/StarPrint001.java
@@ -0,0 +1,12 @@
+package pkg02;
+
+public class StarPrint001 {
+	public static void main(String args[]){
+		for(int i=1;i<=12;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
