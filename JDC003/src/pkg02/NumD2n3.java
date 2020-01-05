77bb386f94331c3e292dfe2a39f0f1fe7f883f27
Prad <prad.j.life@gmail.com>
2020-01-06 03:01:48 +0530

    LCM001

---
+++ JDC003/src/pkg02/NumD2n3.java
@@ -0,0 +1,19 @@
+package pkg02;
+
+public class NumD2n3 {
+	public static void main(String args[]){
+		int a=25;
+		int b=5;
+		int j=0;
+				
+		for(int i=1;i<=(a*b);i++){
+			if(i%a==0&&i%b==0){
+				j=i;
+				System.out.println(j);
+				break;
+			}
+			
+		}
+		
+	}
+}
