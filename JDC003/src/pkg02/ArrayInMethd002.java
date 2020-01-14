c8365c4f0fc0d217b23dc4308e017034877750c7
Prad <prad.j.life@gmail.com>
2020-01-14 15:57:15 +0530

    ArrayInMthd003

---
+++ JDC003/src/pkg02/ArrayInMethd002.java
@@ -0,0 +1,16 @@
+package pkg02;
+
+public class ArrayInMethd002 {
+	public static void main(String args[]){
+		int ar[]={3,2,5};
+		
+		prnt(ar);
+		
+	}
+	public static void prnt(int z[]){
+		for(int x:z){
+			System.out.println(x);
+		}
+	}
+	
+}
