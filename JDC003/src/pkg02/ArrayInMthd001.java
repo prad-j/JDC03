d7a7c0b56b4e94ebbc57f41961d22ec8d6acc5d5
Prad <prad.j.life@gmail.com>
2020-01-14 15:25:54 +0530

    ArrayInMethod

--- JDC003/src/pkg02/ArrayInMthd001.java
+++ JDC003/src/pkg02/ArrayInMthd001.java
@@ -3,10 +3,16 @@ package pkg02;
 public class ArrayInMthd001 {
 	public static void main(String args[]){
 		int ar[]={3,5,1,6,4,2,3};
+		int ar1[]={4,1,4,2,3,7};
 		
 		change(ar);
-		
 		prnt(ar);
+		
+		System.out.println();
+		
+		change(ar1);
+		prnt(ar1);
+		
 		
 	}
 	public static void change(int x[]){
@@ -16,7 +22,7 @@ public class ArrayInMthd001 {
 	}
 	public static void prnt(int z[]){
 		for(int v=0;v<z.length;v++){
-			System.out.println(z[v]);
+			System.out.print(z[v]+" ");
 		}
 	}
 
