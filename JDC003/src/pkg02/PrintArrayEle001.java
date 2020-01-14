c8c1416e91dba3d391683ae605ba2c9e8102641b
Prad <prad.j.life@gmail.com>
2020-01-15 03:53:39 +0530

    ArrayPrint

---
+++ JDC003/src/pkg02/PrintArrayEle001.java
@@ -0,0 +1,11 @@
+package pkg02;
+
+public class PrintArrayEle001 {
+	public static void main(String args[]){
+		int ar[]={2,3,1,5,4,6};
+		
+		for(int c=0;c<ar.length;c++){
+			System.out.print(ar[c]+" ");
+		}
+	}
+}
