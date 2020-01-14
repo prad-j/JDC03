aef3545f61a7929547d8df4df5aac23fbf7a1c5e
Prad <prad.j.life@gmail.com>
2020-01-15 03:55:02 +0530

    EnhancedForLoop

---
+++ JDC003/src/pkg02/EnhanForLoop003.java
@@ -0,0 +1,12 @@
+package pkg02;
+
+public class EnhanForLoop003 {
+	public static void main(String args[]){
+		int ar[]={5,1,3,6,7,2,1};
+	
+		
+		for(int s:ar){
+			System.out.print(s+" ");
+		}
+	}
+}
