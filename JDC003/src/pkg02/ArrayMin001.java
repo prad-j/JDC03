09fd9d54234e09858d65ed37ff8b2ae6a59a3515
Prad <prad.j.life@gmail.com>
2020-01-23 23:02:03 +0530

    ArrayMin

---
+++ JDC003/src/pkg02/ArrayMin001.java
@@ -0,0 +1,15 @@
+package pkg02;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import java.util.Collections;
+
+public class ArrayMin001 {
+	public static void main(String args[]){
+		Integer arr[] ={55,6,68,98,32,23};
+		
+		int min = Collections.min(Arrays.asList(arr));
+		System.out.println("Minimum value in array is "+min);
+	}
+}
