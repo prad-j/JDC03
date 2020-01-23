baaf1b6ac2c82b23b3ed433834ccb63363d379ee
Prad <prad.j.life@gmail.com>
2020-01-23 21:23:36 +0530

    PrintCharArray

---
+++ JDC003/src/pkg02/Test02.java
@@ -0,0 +1,8 @@
+package pkg02;
+
+public class Test02 {
+	public static void main(String args[]){
+		char [] arr = {'P','r','a','m','o','d',' ','S','a','l','u','n','k','e','.'};
+		System.out.print(arr);
+	}
+}
