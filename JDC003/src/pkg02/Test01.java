40d7075e2fad5f87d31af3b6934de255e547ba5c
Prad <prad.j.life@gmail.com>
2020-01-23 21:10:06 +0530

    Test01

---
+++ JDC003/src/pkg02/Test01.java
@@ -0,0 +1,10 @@
+package pkg02;
+
+public class Test01 {
+	public static void main(String args[]){ 
+		        char[] a= {'s','h','u','b','h','a','m',' ','j','a','d','o','n'};  
+		        System.out.println("Printing char array value...");  
+		        System.out.print(a); //out is the object class PrintStream  
+		    }  
+	
+}
