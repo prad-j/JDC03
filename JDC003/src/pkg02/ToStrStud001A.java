37038b34777e7c71868370d6bf4e349e4cc4ea1c
Prad <prad.j.life@gmail.com>
2020-01-23 18:47:40 +0530

    ToString002

---
+++ JDC003/src/pkg02/ToStrStud001A.java
@@ -0,0 +1,11 @@
+package pkg02;
+
+public class ToStrStud001A {
+	public static void main(String args[]){  
+		ToStrStud001B s1=new ToStrStud001B(101,"Raj","lucknow");  
+		ToStrStud001B s2=new ToStrStud001B(102,"Vijay","ghaziabad");  
+		     
+		   System.out.println(s1);//compiler writes here s1.toString()  
+		   System.out.println(s2);//compiler writes here s2.toString()  
+		 }  
+}
---
+++ JDC003/src/pkg02/ToStrStud001B.java
@@ -0,0 +1,14 @@
+package pkg02;
+
+public class ToStrStud001B { 
+		 int rollno;  
+		 String name;  
+		 String city;  
+		  
+		 ToStrStud001B(int rollno, String name, String city){  
+		 this.rollno=rollno;  
+		 this.name=name;  
+		 this.city=city;  
+		 }  
+		 
+}
