c604eadb23aa968e985fd478f358955baf379427
Prad <prad.j.life@gmail.com>
2020-01-23 23:56:37 +0530

    StringPrint

---
+++ JDC003/src/pkg02/StringPrint.java
@@ -0,0 +1,11 @@
+package pkg02;
+
+public class StringPrint {
+	public static void main(String args[]){
+		String s1 = "I am Prad.";
+		String s2 = "I am preparing for Java.";
+		
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
