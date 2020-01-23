ed9406002989ec2990ea80f848a63e5a574ccec9
Prad <prad.j.life@gmail.com>
2020-01-23 21:30:43 +0530

    PrintCharArray

---
+++ JDC003/src/pkg02/Test03.java
@@ -0,0 +1,10 @@
+package pkg02;
+
+public class Test03 {
+	public static void main(String args[]){
+		String [] a = {"Pramod", "Salunke"};
+		System.out.println(a);
+		System.out.println();
+		System.out.println(a[0]+" "+a[1]+".");
+	}
+}
