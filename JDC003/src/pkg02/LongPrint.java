29fa10689607c4b0a8082c0b19a2e16850b69df5
Prad <prad.j.life@gmail.com>
2020-01-23 23:41:13 +0530

    LongPrint

---
+++ JDC003/src/pkg02/LongPrint.java
@@ -0,0 +1,11 @@
+package pkg02;
+
+public class LongPrint {
+	public static void main(String args[]){
+		long l = 1003199712l;
+		int a = 124;
+		
+		System.out.println(l);
+		System.out.println(a);
+	}
+}
