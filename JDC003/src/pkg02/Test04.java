87e0117eae738729f6ed5f68776e2949d63a4394
Prad <prad.j.life@gmail.com>
2020-01-24 22:48:45 +0530

    Test04

--- JDC003/src/pkg02/Test04.java
+++ JDC003/src/pkg02/Test04.java
@@ -2,7 +2,7 @@ package pkg02;
 
 public class Test04 {
 	public static void main(String args[]){
-		Test04B obj =new Test04B(5);
+		Test04B obj =new Test04B(7);
 		
 		System.out.println(obj.x);
 	}
--- JDC003/src/pkg02/Test04B.java
+++ JDC003/src/pkg02/Test04B.java
@@ -3,7 +3,7 @@ package pkg02;
 public class Test04B {
 	int x;
 	
-	public Test04B(int x){
-		this.x = x;
+	public Test04B(int y){
+		x = y;
 	}
 }
