02f0f23b862f2b8a44059dbd20090de86fb7d4d9
Prad <prad.j.life@gmail.com>
2020-01-25 00:47:33 +0530

    toString

--- JDC003/src/pkg02/ToString006A.java
+++ JDC003/src/pkg02/ToString006A.java
@@ -2,8 +2,8 @@ package pkg02;
 
 public class ToString006A {
 	public static void main(String args[]){
-		ToString006B obj=new ToString006B(4,5,6);
+		ToString006B obj=new ToString006B(4,5,7);
 		
-		System.out.println(obj.toString());
+		System.out.println(obj);
 	}
 }
