6566349c949327aa3940251e9c5a4401079ee60b
Prad <prad.j.life@gmail.com>
2020-01-24 16:28:58 +0530

    Constructor this

---
+++ JDC003/src/pkg02/Test04.java
@@ -0,0 +1,9 @@
+package pkg02;
+
+public class Test04 {
+	public static void main(String args[]){
+		Test04B obj =new Test04B(5);
+		
+		System.out.println(obj.x);
+	}
+}
---
+++ JDC003/src/pkg02/Test04B.java
@@ -0,0 +1,9 @@
+package pkg02;
+
+public class Test04B {
+	int x;
+	
+	public Test04B(int x){
+		this.x = x;
+	}
+}
