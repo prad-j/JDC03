692e31d6d833f615e9a689bbff05b4323c92b839
Prad <prad.j.life@gmail.com>
2020-01-24 17:47:01 +0530

    Test

---
+++ JDC003/src/pkg02/Constr001A.java
@@ -0,0 +1,10 @@
+package pkg02;
+
+public class Constr001A {
+	public static void main(String args[]){
+		
+		Constr001B obj = new Constr001B(25);
+		
+		obj.pnt();
+		}
+}
---
+++ JDC003/src/pkg02/Constr001B.java
@@ -0,0 +1,18 @@
+package pkg02;
+
+public class Constr001B {
+	private int x;
+	
+	public Constr001B (int y){
+		x=y;
+	}
+	
+	public int getV(){
+		return x;
+	}
+	
+	
+	public void pnt(){
+		System.out.println(x);
+	}
+}
