063a3e4f81654808cad64c821f94a3449e3bb37b
Prad <prad.j.life@gmail.com>
2020-01-24 17:26:26 +0530

    Test

---
+++ JDC003/src/pkg02/Test06A.java
@@ -0,0 +1,9 @@
+package pkg02;
+
+public class Test06A {
+	public static void main(String args[]){
+		Test06B obj = new Test06B();
+		obj.setV(7);
+		obj.prt();
+	}
+}
---
+++ JDC003/src/pkg02/Test06B.java
@@ -0,0 +1,13 @@
+package pkg02;
+
+public class Test06B {
+	int x=0;
+	int y=14;
+	
+	public void setV(int y){
+		x=y;
+	}
+	public void prt(){
+		System.out.println(x);
+	}
+}
