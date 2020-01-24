e425f28deaf8a6ad9596313c22e440a17c4add8c
Prad <prad.j.life@gmail.com>
2020-01-24 16:51:03 +0530

    ConstructorThis

---
+++ JDC003/src/pkg02/Test05A.java
@@ -0,0 +1,10 @@
+package pkg02;
+
+public class Test05A {
+	public static void main(String args[]){
+		
+		Test05B obj=new Test05B(5);
+		
+		System.out.println(obj.x);
+	}
+}
---
+++ JDC003/src/pkg02/Test05B.java
@@ -0,0 +1,9 @@
+package pkg02;
+
+public class Test05B {
+	int x;
+	
+	public Test05B(int x){
+		x=8;
+	}
+}
