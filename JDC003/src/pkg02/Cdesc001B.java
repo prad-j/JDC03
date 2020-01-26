4f508813b696197ca62ebb6f9c5a8455aa1178af
Prad <prad.j.life@gmail.com>
2020-01-26 14:30:55 +0530

    Enum

---
+++ JDC003/src/pkg02/Cdesc001B.java
@@ -0,0 +1,23 @@
+package pkg02;
+
+public enum Cdesc001B {
+	Virat("Athlet", "31"),
+	Sundar("Techie", "46"),
+	Narendra("Politics", "69"),
+	Ranbir("Acting", "36");
+	
+	private final String prof;
+	private String age;
+	
+	Cdesc001B (String pro, String years){
+		prof=pro;
+		age=years;
+	}
+	public String getPro(){
+		return prof;
+	}
+	public String getYears(){
+		return age;
+	}
+
+}
