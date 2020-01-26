e37db0135d3faf7615e0bda95447e0598a18e4b2
Prad <prad.j.life@gmail.com>
2020-01-27 00:51:19 +0530

    SwitchCase

---
+++ JDC003/src/pkg02/SwitchCase001.java
@@ -0,0 +1,26 @@
+package pkg02;
+
+public class SwitchCase001 {
+	public static void main(String args[]){
+		int age=3;
+		
+		switch (age){
+		case 1:
+			System.out.println("Age is 1");
+			break;
+		case 2:
+			System.out.println("Age is 2");
+			break;
+		case 3:
+			System.out.println("Age is 3");
+			break;
+		case 4:
+			System.out.println("Age is 4");
+			break;
+		default:
+			System.out.println("Check input");
+			break;
+		}
+		
+	}
+}
