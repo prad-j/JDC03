0c9e5389c50f81c7fe3347405e9aa5c645feb80f
Prad <prad.j.life@gmail.com>
2020-01-27 00:56:46 +0530

    switchcase

---
+++ JDC003/src/pkg02/SwitchCase002.java
@@ -0,0 +1,22 @@
+package pkg02;
+
+public class SwitchCase002 {
+	public static void main(String args[]){
+		int a =2;
+		
+		switch (a){
+		case 1:
+			System.out.println("a is 1");
+			break;
+		case 2:
+			System.out.println("a is 2");
+			break;
+		case 3:
+			System.out.println("a is 3");
+			break;
+		default:
+			System.out.println("default");
+			break;
+		}
+	}
+}
