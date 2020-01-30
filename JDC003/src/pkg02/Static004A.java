e45e7578951381b1885f2c197b7fb986442d2e1f
Prad <prad.j.life@gmail.com>
2020-01-31 04:37:35 +0530

    Static

---
+++ JDC003/src/pkg02/Static004A.java
@@ -0,0 +1,12 @@
+package pkg02;
+
+public class Static004A {
+	public static void main(String args[]){
+		Static004B obj = new Static004B("Virat", "Kohli");
+		Static004B obj1 = new Static004B("Rohit", "Sharma");
+		Static004B obj2 = new Static004B("KL", "Rahul");
+		Static004B obj3 = new Static004B("Manish", "Pandya");
+		Static004B obj4 = new Static004B("Shreyas", "Ayyar");
+		Static004B obj5 = new Static004B("Jasprit", "Bumrah");
+	}
+}
