3db9e497a39d8d393ede9fcc55c2ea1955602752
Prad <prad.j.life@gmail.com>
2020-01-31 04:08:21 +0530

    Static variable

---
+++ JDC003/src/pkg02/Static002.java
@@ -0,0 +1,12 @@
+package pkg02;
+
+public class Static002 {
+	public static void main(String args[]){
+		Static001 obj = new Static001("Alia", "Bhat");
+		Static001 obj01 = new Static001("Disha", "Patni");
+		Static001 obj02 = new Static001("Sara", "Khan");
+		Static001 obj03 = new Static001("Katrina", "Kaif");
+		Static001 obj04 = new Static001("Tapsi", "Pannu");
+		
+	}
+}
