8b6c819e5eb8b894ccc964e829c0c15508b6234b
Prad <prad.j.life@gmail.com>
2020-01-31 05:15:59 +0530

    Static variable

---
+++ JDC003/src/pkg02/Static005A.java
@@ -0,0 +1,12 @@
+package pkg02;
+
+public class Static005A {
+	public static void main(String args[]){
+		Static005B obj = new Static005B("Sunil", "Gavaskar");
+		Static005B obj1 = new Static005B("Sachin", "Tendulkar");
+		Static005B obj2 = new Static005B("Dilip", "Vengasarkar");
+		Static005B obj3 = new Static005B("Kapil", "Dev");
+		Static005B obj4 = new Static005B("Ravi", "Shastri");
+		Static005B obj5 = new Static005B("Saurav", "Ganguli");
+	}
+}
