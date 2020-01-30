9820ece006cafee0bcc2268a16adeab931c4a132
Prad <prad.j.life@gmail.com>
2020-01-31 03:49:05 +0530

    edit

--- JDC003/src/pkg02/FinalTest.java
+++ JDC003/src/pkg02/FinalTest.java
@@ -3,7 +3,7 @@ package pkg02;
 public class FinalTest {
 	public static void main(String args[]){
 		final int i = 10;
-		//i = i+10; (if value of the "final" variable is assigned, it throws error) 
+		//i = i+20; (if value of the "final" variable is assigned, it throws error) 
 		System.out.println(i);
 	}
 }
