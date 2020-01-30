ed013ab967227fde67adccb72bc396d1b0d91754
Prad <prad.j.life@gmail.com>
2020-01-31 03:28:07 +0530

    Final Variable

---
+++ JDC003/src/pkg02/FinalTest.java
@@ -0,0 +1,9 @@
+package pkg02;
+
+public class FinalTest {
+	public static void main(String args[]){
+		final int i = 10;
+		//i = i+10; (if value of the "final" variable is assigned, it throws error) 
+		System.out.println(i);
+	}
+}
