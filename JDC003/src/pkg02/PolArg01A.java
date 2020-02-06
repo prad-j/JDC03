eb60d1225601187ef1ef074b9744bf67cbca7256
Prad <prad.j.life@gmail.com>
2020-02-06 20:38:51 +0530

    PolyMArgument

---
+++ JDC003/src/pkg02/PolArg01A.java
@@ -0,0 +1,10 @@
+package pkg02;
+
+public class PolArg01A {
+	public static void main(String args[]){
+		PolArg01E obj = new PolArg01E();
+		PolArg01D pass = new PolArg01D();
+		
+		obj.intest(pass);
+	}
+}
