39141c30fc173f3676eef4f9b0d056d58bde35bd
Prad <prad.j.life@gmail.com>
2020-02-06 20:43:54 +0530

    PolyMArguments

--- JDC003/src/pkg02/PolArg01A.java
+++ JDC003/src/pkg02/PolArg01A.java
@@ -4,7 +4,12 @@ public class PolArg01A {
 	public static void main(String args[]){
 		PolArg01E obj = new PolArg01E();
 		PolArg01D pass = new PolArg01D();
+		PolArg01C pass1 = new PolArg01C();
+		PolArg01B pass2 = new PolArg01B();
 		
 		obj.intest(pass);
+		obj.intest(pass1);
+		obj.intest(pass2);
+		
 	}
 }
